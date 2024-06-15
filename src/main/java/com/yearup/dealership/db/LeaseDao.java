7e19d6257fcdb6c1f9425e39879372b9a43a2f6c
HasnainArifYU <harif@pgh.yearup.org>
2024-06-15 15:25:00 -0400

    leasedao

--- src/main/java/com/yearup/dealership/db/LeaseDao.java
+++ src/main/java/com/yearup/dealership/db/LeaseDao.java
@@ -2,6 +2,7 @@ package com.yearup.dealership.db;
 
 import com.yearup.dealership.models.LeaseContract;
 
+import java.sql.Date;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -16,5 +17,26 @@ public class LeaseDao {
 
     public void addLeaseContract(LeaseContract leaseContract) {
         // TODO: Implement the logic to add a lease contract
+        String sql = "INSERT INTO lease_contracts (VIN, lease_start, lease_end, monthly_payment) VALUES (?, ?, ?, ?)";
+
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS)) {
+            preparedStatement.setString(1, leaseContract.getVin());
+            preparedStatement.setDate(2, Date.valueOf(leaseContract.getLeaseStart()));
+            preparedStatement.setDate(3, Date.valueOf(leaseContract.getLeaseEnd()));
+            preparedStatement.setDouble(4, leaseContract.getMonthlyPayment());
+            int rows = preparedStatement.executeUpdate();
+            if (rows > 0) {
+                try (var generatedKeys = preparedStatement.getGeneratedKeys()) {
+                    if (generatedKeys.next()) {
+                        leaseContract.setContractId(generatedKeys.getInt(1));
+                    }
+                }
+            }
+            System.out.println("Rows inserted: " + rows);
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
+
